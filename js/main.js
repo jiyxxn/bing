32d094de50b98a37decb1889818a07e28762d208
jiyxxn <132261550+jiyxxn@users.noreply.github.com>
2023-07-19 16:08:01 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ mainMenu.forEach(mainMenu => {
 })
 
 
-// 6️⃣ 언어설정
+// 2️⃣ 언어설정
 const btnLang = document.querySelector(".language > span")
 const lang = document.querySelector(".language > ul")
 
@@ -48,52 +48,112 @@ btnLang.addEventListener("click", e => {
   lang.classList.toggle("active");
 })
 
-// 4️⃣ 주메뉴 햄버거 버튼 이벤트
+// 3️⃣ 주메뉴 햄버거 버튼 이벤트
 const gnb = document.querySelector("nav.gnb");
 const btnHam = document.querySelector("div.hamburger > a");
 const body = document.querySelector("body");
 const bg = document.querySelector("div.bg");
 const gnbAct = document.querySelector("nav.gnbActive");
 
+
 btnHam.addEventListener("click", e => {
+  e.preventDefault();
+
   gnb.classList.toggle("on");
   body.classList.toggle("on");
   bg.classList.toggle("on");
   gnbAct.classList.toggle("on");
   e.currentTarget.parentElement.classList.toggle("on");
+
   if(gnb.classList.contains("on")){
     lang.classList.remove("active");
   }
 })
-// 5️⃣ 탑버튼 (탑버튼 모양 변경은 1번 스크롤이벤트에)
-const btnTop = document.querySelector(".btn_top > a");
-const btnTopOn = document.querySelector(".btn_top > a.on");
+
+// 4️⃣ 탑버튼
+const btnTop = document.querySelectorAll(".btn_top > a");
+console.log(btnTop)
 
 let bodyHeight = body.offsetHeight;
+document.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
 
-btnTop.addEventListener("click", (e) => { 
-  e.preventDefault();
-  if(btnTop.classList.contains("on")){
-    window.scroll({
-      top: bodyHeight,
-      left: 0,
-      behavior: "smooth"
-    });
-  } else {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: "smooth"
-    });
+  if(scroll <= 0){
+    btnTop.forEach(item => {
+      item.classList.add("on");
+    })
+  } else if(scroll < 2000) {
+    btnTop.forEach(item => {
+      item.classList.remove("on");
+    })
   }
-});
-// 2️⃣ cont1 행복한 빙그레의 맛 기차
-const bingTrain = document.querySelector(".menu > ul");
-const bingTrainEle = document.querySelectorAll(".menu > ul > li");
+})
+
+btnTop.forEach(item => {
+  item.addEventListener("click", (e) => { 
+    e.preventDefault();
+    if(item.classList.contains("on")){
+      window.scroll({
+        top: bodyHeight,
+        left: 0,
+        behavior: "smooth"
+      });
+    } else {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    }
+  });
+})
+
+
+// 5️⃣ 모바일/태블릿 헤더 메뉴
+const m_btnHam = document.querySelector("div.m_hamburger > a");
+const m_btnClose = document.querySelector(".m_topLink > li:last-of-type > a");
+const mobMenu = document.querySelector(".m_header_inner");
+
+m_btnHam.addEventListener("click", e=> {
+  mobMenu.classList.add("on");
+  bg.classList.add("on");
+  body.classList.add("on");
+})
+m_btnClose.addEventListener("click", e=> {
+  e.preventDefault();
+  mobMenu.classList.remove("on");
+  bg.classList.remove("on");
+  body.classList.remove("on");
+})
+
+const m_btnLang = document.querySelector(".m_language > span");
+const m_lang = document.querySelector(".m_language > ul");
+
+m_btnLang.addEventListener("click", e=> {
+  e.preventDefault();
+  m_lang.classList.toggle("active");
+})
+
+const m_subLi = document.querySelectorAll("ul.m_menu > li");
+
+for(var i=0; i<m_subLi.length; i++){
+  m_subLi[i].addEventListener("click", e=> {
+    // m_subLi.forEach(item => {
+    //   item.classList.remove("active");
+    // })
+    e.currentTarget.classList.toggle("active");
+  })
+}
+// 6️⃣ cont1 행복한 빙그레의 맛 기차
+const bingTrain = document.querySelector(".menu > ul:first-of-type");
+const bingTrainEle = document.querySelectorAll(".menu > ul:first-of-type > li");
 const bing_prevBtn = document.querySelector(".menu > .menu_arrow > a:first-child");
 const bing_nextBtn = document.querySelector(".menu > .menu_arrow > a:last-child");
+console.log(bingTrainEle)
+let eleWidth = document.querySelector(".menu > ul > li").clientWidth;
 
-// 메뉴 슬라이드
+
+// ❗ 메뉴 슬라이드
 trainNum = 0;
 
 function trainNext(train, element, nextBtn, prevBtn){
@@ -102,7 +162,7 @@ function trainNext(train, element, nextBtn, prevBtn){
     trainNum = element.length - 6;
     nextBtn.classList.remove("on");
   }
-    train.style.transform = `translateX(${trainNum * -259}px)`;
+    train.style.transform = `translateX(${-trainNum * (eleWidth+17)}px)`;
     prevBtn.classList.add("on");
 }
 function trainPrev(train, nextBtn, prevBtn){
@@ -111,20 +171,22 @@ function trainPrev(train, nextBtn, prevBtn){
     trainNum = 0;
     prevBtn.classList.remove("on");
   }
-  train.style.transform = `translateX(${trainNum * -259}px)`;
+  train.style.transform = `translateX(${-trainNum * (eleWidth+17)}px)`;
   nextBtn.classList.add("on")
 }
 
 bing_nextBtn.addEventListener("click", e => {
+  e.preventDefault();
   trainNext(bingTrain, bingTrainEle, bing_nextBtn, bing_prevBtn);
 });
 
 bing_prevBtn.addEventListener("click", e => {
+  e.preventDefault();
   trainPrev(bingTrain, bing_nextBtn, bing_prevBtn);
 });
 
 
-// 행복한 빙그레의 맛 hover 효과 delay 리셋
+// ❗ 행복한 빙그레의 맛 hover 효과 delay 리셋
 for(i=0; i<bingTrainEle.length; i++){
 
   bingTrainEle[i].addEventListener("mouseover", e =>{
@@ -138,24 +200,53 @@ for(i=0; i<bingTrainEle.length; i++){
 }
 
 
-// 3️⃣ cont2 브랜드 기차
-const brandTrain = document.querySelector(".brand > ul")
-const brandTrainEle = document.querySelectorAll(".brand > ul > li")
+// 7️⃣ cont2 브랜드 기차
+const brandTrain = document.querySelector(".brand > ul:first-of-type")
+const brandTrainEle = document.querySelectorAll(".brand > ul:first-of-type > li")
 const brand_prevBtn = document.querySelector(".brand > .menu_arrow > a:first-child")
 const brand_nextBtn = document.querySelector(".brand > .menu_arrow > a:last-child")
 
 brand_nextBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   trainNext(brandTrain, brandTrainEle, brand_nextBtn, brand_prevBtn);
 });
 brand_prevBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   trainPrev(brandTrain, brand_nextBtn, brand_prevBtn);
 });
 
+// ❗ content1,2 mob.ver
+const bing_pagnation = document.querySelectorAll(".menu > ul:last-of-type > li");
+const brand_pagnation = document.querySelectorAll(".brand > ul:last-of-type > li");
+let bdWidth = body.offsetWidth;
+
+function mobPagnation(circle, count, train){
+  circle[0].addEventListener("click", e=>{
+    e.preventDefault();
+    circle.forEach(item => {
+      item.classList.remove("active");
+    })
+    e.currentTarget.classList.add("active");
+    train.style.transform = `translateX(${0}px)`;
+  })
+  circle[1].addEventListener("click", e=>{
+    e.preventDefault();
+    circle.forEach(item => {
+      item.classList.remove("active");
+    })
+    e.currentTarget.classList.add("active");
+    train.style.transform = `translateX(${-count * (eleWidth+17)}px)`;
+  })
+}
+
+if(bdWidth < 769){
+  mobPagnation(bing_pagnation, 2, bingTrain);
+  mobPagnation(brand_pagnation, 1, brandTrain);
+}
 
 
 
-
-//  scroll event
+// 8️⃣ scroll event
 function wheel (element){
   for(let i=0; i<bingTrainEle.length; i++){
     element[i].parentElement.classList.add("on");
@@ -167,30 +258,53 @@ let cont4 = document.querySelector("div.cont4_inner");
 document.addEventListener("scroll", () => {
   let scroll = window.scrollY;
 
-  if(scroll >= 100 && scroll < 750){
-    wheel(bingTrainEle);
-  } else if(scroll >= 750 && scroll < 1450){
-    wheel(brandTrainEle);
-  } else if(scroll >= 1450 && scroll < 2600){
-    cont3.classList.add("on");
-  } else if(scroll >= 2600){
-    cont4.classList.add("on");
+  function scrollY(a, b, c, d){
+    if(scroll >= a && scroll < b){
+      wheel(bingTrainEle);
+    } else if(scroll >= b && scroll < c){
+      wheel(brandTrainEle);
+    } else if(scroll >= c && scroll < d){
+      cont3.classList.add("on");
+    } else if(scroll >= d){
+      cont4.classList.add("on");
+    }
   }
+
+  if(bdWidth <= 768){
+    // 모바일
+    scrollY(10, 550, 1300, 2280);
+
+  } else if(bdWidth > 768 && bdWidth <= 1024){
+    // 태블릿
+    scrollY(10, 550, 1150, 1900);
+  } else {
+    // PC
+    scrollY(100, 750, 1450, 2600);
+  }
+
 })
+// ❗ 모바일, 행복한 빙그레의 맛만 0.5초 후에 뜨기
+if(bdWidth <= 768){
+  let reload = setTimeout(()=>{
+    bingTrain.classList.add("on");
+  }, 500);
+}
 
-// 오토배너
+// 9️⃣ 배너
 
-// let bannerBox = document.querySelector("section.banner");
-let bannerFrame = document.querySelector("div.banner_frame");
-let bannerParent = document.querySelector("div.banner_frame > ul");
-let banners = document.querySelectorAll("div.banner_frame > ul > li");
-
+const bannerFrame = document.querySelector("div.banner_frame");
+const bannerParent = document.querySelector("div.banner_frame > ul");
+const banners = document.querySelectorAll("div.banner_frame > ul > li");
+	
 const firstEl = bannerParent.firstElementChild;
+const lastEl = bannerParent.lastElementChild;
 let cloneFirst = firstEl.cloneNode(true);
+let cloneLast = lastEl.cloneNode(true);
 	
 bannerParent.appendChild(cloneFirst);
+bannerParent.insertBefore(cloneLast, bannerParent.firstElementChild);
 
-bannerFrame.style.width = `${100*(banners.length+1)}%`;
+bannerFrame.style.width = `${100*(banners.length+2)}%`;
 
 let bnnW = firstEl.offsetWidth;
 
@@ -198,15 +312,61 @@ let bnnW = firstEl.offsetWidth;
 let bnnNum = 0;
 let lastNum = banners.length;
 
-function autoBanner(){
+const next = document.querySelector("div.banner_roll > a.next");
+const prev = document.querySelector("div.banner_roll > a.prev");
+const rolling = document.querySelectorAll("div.banner_roll > ul > li");
+const play = document.querySelector("div.banner_roll > p > a");
+
+bannerFrame.style.transform = `translateX(${-bnnW}px)`
+
+next.addEventListener("click", e=> {
+  e.preventDefault();
   bnnNum++
-  bannerFrame.style.transform = `translateX(${-bnnNum * bnnW}px)`
+  bannerFrame.style.transform = `translateX(${-bnnW * (bnnNum+1)}px)`
   bannerFrame.style.transition = "800ms"
 
   if(bnnNum == lastNum){
     setTimeout(function(){
       bannerFrame.style.transition = "0ms"
-      bannerFrame.style.transform = `translateX(0px)`
+      bannerFrame.style.transform = `translateX(${-bnnW}px)`
+    }, 800)
+    bnnNum = 0;
+  }
+  rolling.forEach(item => {
+    item.classList.remove("on");
+  })
+  rolling[bnnNum].classList.add("on");
+})
+prev.addEventListener("click", e=> {
+  e.preventDefault();
+  bnnNum --
+  bannerFrame.style.transform = `translateX(${-bnnW * (bnnNum+1)}px)`
+  bannerFrame.style.transition = "800ms"
+  
+  if(bnnNum == -1){
+    setTimeout(function(){
+      bannerFrame.style.transition = "0ms"
+      bannerFrame.style.transform = `translateX(${-bnnW * lastNum}px)`
+    }, 800)
+    bnnNum = lastNum-1;
+  }
+  rolling.forEach(item => {
+    item.classList.remove("on");
+  })
+  rolling[bnnNum].classList.add("on");
+})
+
+
+// ❗ 오토배너
+function autoBanner(){
+  bnnNum++
+  bannerFrame.style.transform = `translateX(${-bnnW * (bnnNum+1)}px)`
+  bannerFrame.style.transition = "800ms"
+
+  if(bnnNum == lastNum){
+    setTimeout(function(){
+      bannerFrame.style.transition = "0ms"
+      bannerFrame.style.transform = `translateX(${-bnnW}px)`
     }, 800)
     bnnNum = 0;
   }
@@ -214,4 +374,39 @@ function autoBanner(){
 }
 let autoBnn = setTimeout(autoBanner, 4000);
 
+// ❗ play/pause
+let btnP = true;
+
+play.addEventListener("click", e=> {
+  e.preventDefault();
+  if(btnP){
+    play.classList.add("pause");
+    clearTimeout(autoBnn);
+    btnP = false;
+  } else {
+    play.classList.remove("pause");
+    autoBnn = setTimeout(autoBanner, 4000);
+    btnP = true;
+  }
+})
+
+// ❗ 페이지네이션 클릭이벤트
+for(let i=0; i<rolling.length; i++){
+  rolling[i].addEventListener("click", e => {
+    e.preventDefault();
+    bannerFrame.style.transform = `translateX(${-bnnW * (i+1)}px)`
+    bannerFrame.style.transition = "800ms"
+    rolling.forEach(item => {
+      item.classList.remove("on");
+    })
+    e.currentTarget.classList.add("on");
+    play.classList.add("pause")
+    clearTimeout(autoBnn);
+    btnP = false; 
+  })
+}
+
+
+
 });
+// window.onload
